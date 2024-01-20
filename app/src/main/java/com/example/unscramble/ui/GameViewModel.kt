5edae812f567114524f392f572b46ef6cd2018a5
onishi taisei <taisei.developer@gmail.com>
2024-01-20 15:04:57 +0900

    🚀 feat: ViewModelを作成

---
+++ app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -0,0 +1,44 @@
+package com.example.unscramble.ui
+
+import androidx.compose.runtime.MutableState
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import com.example.unscramble.data.allWords
+
+private val _uiState = MutableStateFlow(GameUiState())
+val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+
+class GameViewModel : ViewModel() {
+    private lateinit var currentWord: String
+    private var usedWords: MutableSet<String> = mutableSetOf()
+
+    init {
+        resetGame()
+    }
+
+    private fun pickRandomWordAndShuffle(): String {
+        currentWord = allWords.random()
+        if (usedWords.contains(currentWord)) {
+            return pickRandomWordAndShuffle()
+        } else {
+            usedWords.add(currentWord)
+            return shuffleCurrentWord(currentWord)
+        }
+    }
+
+    private fun shuffleCurrentWord(word: String): String {
+        val tempWord = word.toCharArray()
+        tempWord.shuffle()
+        while (String(tempWord).equals(word)) {
+            tempWord.shuffle()
+        }
+        return String(tempWord)
+    }
+
+    fun resetGame() {
+        usedWords.clear()
+        _uiState.value = GameUiState(currentScrambledWord = pickRandomWordAndShuffle())
+    }
+}
